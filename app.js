8939f60f687698f03c92a1d752debcb1d8e09352
seif0912 <baddredineseif@gmail.com>
2022-06-22 23:39:21 +0100

    database connection and ejs setup

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const mysql = require('mysql')
+const ejs = require('ejs')
+
 const app = express()
 const port = 3000
 
+const db =  mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "contact_form"
+})
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.set('view engine', 'ejs')
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('<h1>Working...</h1>')
+    res.render('index')
+})
+app.get('/contact', (req, res) => {
+    res.render('contact')
 })
 
 app.listen(port, () => {
